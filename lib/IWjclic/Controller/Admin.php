479e9e1d43c8572ed77c28248071bde80eb1805e
Albert Pérez Monfort <aperez16@xtec.cat>
2012-01-24 16:44:47 +0100

    Moving to Zikula 1.3

--- lib/IWjclic/Controller/Admin.php
+++ lib/IWjclic/Controller/Admin.php
@@ -1,111 +1,95 @@
 <?php
-/**
- * Show the main configurable parameters needed to configurate the module jclic
- * @author:     Albert PÃ©rez Monfort (aperezm@xtec.cat)
 
- * @return:	The form with needed to change the parameters
-*/
-function IWjclic_admin_main()
-{
-	$dom = ZLanguage::getModuleDomain('IWjclic');
-	// Security check
-	if (!SecurityUtil::checkPermission('IWjclic::', "::", ACCESS_ADMIN)) {
-		return LogUtil::registerError(__('Sorry! No authorization to access this module.', $dom), 403);
-	}
+class IWjclic_Controller_Admin extends Zikula_AbstractController {
 
-	$jclicUpdatedFiles = ModUtil::getVar('IWjclic','jclicUpdatedFiles');
-	$jclicJarBase = ModUtil::getVar('IWjclic','jclicJarBase');
-	$timeLap = ModUtil::getVar('IWjclic','timeLap');
-	$groupsProAssign = ModUtil::getVar('IWjclic','groupsProAssign');
+    public function postInitialize() {
+        $this->view->setCaching(false);
+    }
 
-	// Create output object
-	$view = Zikula_View::getInstance('IWjclic',false);
+    /**
+     * Show the main configurable parameters needed to configurate the module jclic
+     * @author:     Albert Pérez Monfort (aperezm@xtec.cat)
 
-	$sv = ModUtil::func('IWmain', 'user', 'genSecurityValue');
-	$groups = ModUtil::func('IWmain', 'user', 'getAllGroups', array('sv' => $sv,
-																	'less' => ModUtil::getVar('iw_myrole', 'rolegroup')));
-																	
-	foreach($groups as $group){
-		$checked = false;
-		
-		if(strpos($groupsProAssign,'$'.$group['id'].'$') != false){
-			$checked = true;	
-		}
-		
-		$groupsArray[] = array('id' => $group['id'],
-								'name' => $group['name'],
-								'checked' => $checked);
-	}
+     * @return:	The form with needed to change the parameters
+     */
+    public function main() {
+        // Security check
+        if (!SecurityUtil::checkPermission('IWjclic::', "::", ACCESS_ADMIN)) {
+            throw new Zikula_Exception_Forbidden();
+        }
 
-	if(!file_exists(ModUtil::getVar('IWmain', 'documentRoot').'/'.$jclicUpdatedFiles) || $jclicUpdatedFiles == ''){
-		$view -> assign('noFolder', true);
-	}else{
-		if(!is_writeable(ModUtil::getVar('IWmain', 'documentRoot').'/'.$jclicUpdatedFiles)){
-			$view -> assign('noWriteable', true);
-		}
-	}
+        $noWriteable = false;
+        $noFolder = false;
 
-	$view -> assign('jclicJarBase', $jclicJarBase);
-	$view -> assign('timeLap', $timeLap);
-	$view -> assign('jclicUpdatedFiles', $jclicUpdatedFiles);
-	$view -> assign('groupsArray', $groupsArray);
-	
-	return $view -> fetch('IWjclic_admin_main.htm');
-}
+        $jclicUpdatedFiles = ModUtil::getVar('IWjclic', 'jclicUpdatedFiles');
+        $jclicJarBase = ModUtil::getVar('IWjclic', 'jclicJarBase');
+        $timeLap = ModUtil::getVar('IWjclic', 'timeLap');
+        $groupsProAssign = ModUtil::getVar('IWjclic', 'groupsProAssign');
 
-/**
- * Show the information about the module
- * @author:     Albert PÃ©rez Monfort (aperezm@xtec.cat)
- * @return:	The information about this module
-*/
-function IWjclic_admin_module()
-{
-	$dom = ZLanguage::getModuleDomain('IWjclic');
-	// Create output object
-	$view = Zikula_View::getInstance('IWjclic',false);
+        $sv = ModUtil::func('IWmain', 'user', 'genSecurityValue');
+        $groups = ModUtil::func('IWmain', 'user', 'getAllGroups', array('sv' => $sv,
+                    'less' => ModUtil::getVar('iw_myrole', 'rolegroup')));
 
-	$module = ModUtil::func('IWmain', 'user', 'module_info', array('module_name' => 'IWjclic',
-																'type' => 'admin'));
-																
-	$view -> assign('module', $module);
-	return $view -> fetch('IWjclic_admin_module.htm');
-}
+        foreach ($groups as $group) {
+            $checked = false;
 
-/**
- * Update the module configuration
- * @author:     Albert PÃ©rez Monfort (aperezm@xtec.cat)
- * @param:	Configuration values
- * @return:	The form with needed to change the parameters
-*/
-function IWjclic_admin_updateConf($args)
-{
-	$dom = ZLanguage::getModuleDomain('IWjclic');
-	$jclicJarBase = FormUtil::getPassedValue('jclicJarBase', isset($args['jclicJarBase']) ? $args['jclicJarBase'] : null, 'POST');
-	$timeLap = FormUtil::getPassedValue('timeLap', isset($args['timeLap']) ? $args['timeLap'] : null, 'POST');
-	$groups = FormUtil::getPassedValue('groups', isset($args['groups']) ? $args['groups'] : null, 'POST');
-	$jclicUpdatedFiles = FormUtil::getPassedValue('jclicUpdatedFiles', isset($args['jclicUpdatedFiles']) ? $args['jclicUpdatedFiles'] : null, 'POST');
-	
-	// Security check
-	if (!SecurityUtil::checkPermission('IWjclic::', "::", ACCESS_ADMIN)) {
-		return LogUtil::registerError(__('Sorry! No authorization to access this module.', $dom), 403);
-	}
+            if (strpos($groupsProAssign, '$' . $group['id'] . '$') != false)
+                $checked = true;
 
-	// Confirm authorisation code
-	if (!SecurityUtil::confirmAuthKey()) {
-		return LogUtil::registerAuthidError (ModUtil::url('IWjclic', 'admin', 'main'));
-	}
-	
-	$groupsString = '$';
-	foreach($groups as $group){
-		$groupsString .= '$'.$group.'$';
-	}
 
-	ModUtil::setVar('IWjclic','jclicUpdatedFiles', $jclicUpdatedFiles);
-	ModUtil::setVar('IWjclic','jclicJarBase', $jclicJarBase);
-	ModUtil::setVar('IWjclic','timeLap', $timeLap);
-	ModUtil::setVar('IWjclic','groupsProAssign', $groupsString);
+            $groupsArray[] = array('id' => $group['id'],
+                'name' => $group['name'],
+                'checked' => $checked);
+        }
 
-	LogUtil::registerStatus (__('The module configuration has changed', $dom));
+        if (!file_exists(ModUtil::getVar('IWmain', 'documentRoot') . '/' . $jclicUpdatedFiles) || $jclicUpdatedFiles == '') {
+            $noFolder = true;
+        } else {
+            if (!is_writeable(ModUtil::getVar('IWmain', 'documentRoot') . '/' . $jclicUpdatedFiles))
+                $noWriteable = true;
+        }
 
-	return System::redirect(ModUtil::url('IWjclic', 'admin', 'main'));
+        return $this->view->assign('jclicJarBase', $jclicJarBase)
+                        ->assign('timeLap', $timeLap)
+                        ->assign('jclicUpdatedFiles', $jclicUpdatedFiles)
+                        ->assign('groupsArray', $groupsArray)
+                        ->assign('noFolder', $noFolder)
+                        ->assign('noWriteable', $noWriteable)
+                        ->fetch('IWjclic_admin_main.htm');
+    }
+
+    /**
+     * Update the module configuration
+     * @author:     Albert Pérez Monfort (aperezm@xtec.cat)
+     * @param:	Configuration values
+     * @return:	The form with needed to change the parameters
+     */
+    public function updateConf($args) {
+        $jclicJarBase = FormUtil::getPassedValue('jclicJarBase', isset($args['jclicJarBase']) ? $args['jclicJarBase'] : null, 'POST');
+        $timeLap = FormUtil::getPassedValue('timeLap', isset($args['timeLap']) ? $args['timeLap'] : null, 'POST');
+        $groups = FormUtil::getPassedValue('groups', isset($args['groups']) ? $args['groups'] : null, 'POST');
+        $jclicUpdatedFiles = FormUtil::getPassedValue('jclicUpdatedFiles', isset($args['jclicUpdatedFiles']) ? $args['jclicUpdatedFiles'] : null, 'POST');
+
+        // Security check
+        if (!SecurityUtil::checkPermission('IWjclic::', "::", ACCESS_ADMIN)) {
+            throw new Zikula_Exception_Forbidden();
+        }
+
+        // Confirm authorisation code
+        $this->checkCsrfToken();
+
+        $groupsString = '$';
+        foreach ($groups as $group)
+            $groupsString .= '$' . $group . '$';
+
+        $this->setVar('jclicUpdatedFiles', $jclicUpdatedFiles)
+                ->setVar('jclicJarBase', $jclicJarBase)
+                ->setVar('timeLap', $timeLap)
+                ->setVar('groupsProAssign', $groupsString);
+
+        LogUtil::registerStatus($this->__('The module configuration has changed'));
+
+        return System::redirect(ModUtil::url('IWjclic', 'admin', 'main'));
+    }
+
 }
